b40855ac42dee5ef845001b6e9e8a2d301a9bff1
Germán Aliprandi <galiprandi@gmail.com>
2026-03-29 04:57:27 -0300

    fix: restore UIProvider wrapper in root layout
    Accidentally removed when cleaning up UserSync component

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        <UIProvider>
+          {children}
+        </UIProvider>
       </body>
     </html>
   );
